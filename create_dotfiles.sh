e72d2308ec36d54d3b5beb75586e3afb02794d2d
Alexander Eirich <alexander.eirich.87@gmail.com>
2024-02-11 19:15:06 +0100

    removing bash variant

--- create_dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Bash
-mkdir -p ~/.dotfiles/bash
-rsync ~/.bashrc ~/.bashrc_custom ~/.bash_aliases ~/.dotfiles/bash/
-
-# Vim
-mkdir -p ~/.dotfiles/vim
-rsync ~/.vimrc ~/.vim ~/.dotfiles/vim/
