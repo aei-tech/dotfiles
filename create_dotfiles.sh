c4756472e1f6463a78bef5053c759a0ba2b92188
Alexander Eirich <alexander.eirich.87@gmail.com>
2024-02-11 14:49:11 +0100

    initial vim commit

--- create_dotfiles.sh
+++ create_dotfiles.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # Bash
+mkdir -p ~/.dotfiles/bash
 rsync ~/.bashrc ~/.bashrc_custom ~/.bash_aliases ~/.dotfiles/bash/
 
+# Vim
+mkdir -p ~/.dotfiles/vim
+rsync ~/.vimrc ~/.vim ~/.dotfiles/vim/
