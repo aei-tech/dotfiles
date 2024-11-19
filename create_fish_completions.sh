5e71a26ade8e7bef40009bb8faa9e93d22157881
Alexander Eirich <alexander.eirich.87@gmail.com>
2024-11-19 23:53:53 +0100

    completions

---
+++ create_fish_completions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker completion fish > ~/.config/fish/completions/docker.fish
+k9s completion fish >> ~/.config/fish/completions/k9s.fish
+kubectl completion fish > ~/.config/fish/completions/kubectl.fish
+kubeshark completion fish > ~/.config/fish/completions/kubeshark.fish
+localstack completion fish > ~/.config/fish/completions/localstack
+minikube completion fish > ~/.config/fish/completions/minikube.fish
+trivy completion fish > ~/.config/fish/completions/trivy.fish
